4c92bd01d1b5597014883a7a8886cbbb08f47994
Itachi666 <494521900@qq.com>
2018-07-21 16:34:14 +0800

    Flame1

--- Framework/Common/main.cpp
+++ Framework/Common/main.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include "IApplication.hpp"
 
-int main(){
-    printf("Hello Engine!\n");
+using namespace My;
+
+namespace My {
+    extern IApplication* g_pApp;
+}
+
+int main(int argc, char** argv) {
+    int ret;
+
+    if ((ret=g_pApp->Initialize())!=0) {
+        printf("App Initialize failed, whill exit now.");
+        return ret;
+    }
+
+    while (!g_pApp->IsQuit()) {
+        g_pApp->Tick();
+    }
+
+    g_pApp->Finalize();
+
     return 0;
 }
